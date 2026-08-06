a28416350ab43fd8328c0c51556cc55a7726cc50
bdawit1 <bdawit2005@gmail.com>
2026-08-06 18:04:38 -0400

    Update submission-0.py

--- Data Structures & Algorithms/products-of-array-discluding-self/submission-0.py
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.py
@@ -1,6 +1,14 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-
+        ### nums [1,2,3,4] Let's use 3
+        ### prefix  [1,2,6,24] # multiply every number before 3
+        ### postfix [24,24,12,4] #every number after 3
+        ### for position 1: 1 * 24 = 24 because 2 * 3 * 4 = 24
+        ### for position 2: 1 * 12 = 12 because 1 * 3 * 4 = 12
+        ### for position 3: 2 *  4 = 8  because 1 * 2 * 4 = 8
+        ### for position 4: 6 *  1 = 6  becuase 1 * 2 * 3 = 6
+        ### output [24,12,8,6]
+        
         result = [1] * len(nums) #start at 1 * len of array
 
         prefix = 1
